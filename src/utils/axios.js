461af0c9439600a81496b470ce11349bd6a4bf0c
ramses zeulevoet <ramses@ramsesz.be>
2019-12-23 22:57:40 +0100

    fix formatting

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,16 +1,16 @@
-const axios = require('axios')
-const chalk = require('chalk')
-const oauthSignature = require('oauth-signature')
+const axios = require(`axios`)
+const chalk = require(`chalk`)
+const oauthSignature = require(`oauth-signature`)
 
-const { routes } = require('./routes')
-const { isObjEmpty, slugify } = require('./helpers')
-const { new0AuthParameters } = require('./oAuthParameters')
+const { routes } = require(`./routes`)
+const { isObjEmpty, slugify } = require(`./helpers`)
+const { new0AuthParameters } = require(`./oAuthParameters`)
 
 const log = console.log
 
 // Get list of all forms from GF
 async function getForms(basicAuth, api, baseUrl) {
-    log(chalk.black.bgWhite('Fetching form ids'))
+    log(chalk.black.bgWhite(`Fetching form ids`))
 
     const authParams = new0AuthParameters(api.key)
 
@@ -18,7 +18,7 @@ async function getForms(basicAuth, api, baseUrl) {
 
     try {
         const signature = oauthSignature.generate(
-            'GET',
+            `GET`,
             baseUrl + routes.wp + routes.gf + routes.forms,
             authParams,
             api.secret
@@ -27,7 +27,7 @@ async function getForms(basicAuth, api, baseUrl) {
         result = await axios.get(
             baseUrl + routes.wp + routes.gf + routes.forms,
             {
-                responseType: 'json',
+                responseType: `json`,
                 params: {
                     ...authParams,
                     oauth_signature: signature,
@@ -48,26 +48,28 @@ async function getForms(basicAuth, api, baseUrl) {
 async function getFormFields(basicAuth, api, baseUrl, form) {
     log(chalk.black.bgWhite(`Fetching fields for form ${form.id}`))
 
-    let authParams = new0AuthParameters(api.key)
+    const authParams = new0AuthParameters(api.key)
 
     let result
 
     const apiURL =
-        baseUrl + routes.wp + routes.gf + routes.forms + '/' + form.id
+        baseUrl + routes.wp + routes.gf + routes.forms + `/` + form.id
 
     // Make a new signature
     const signature = oauthSignature.generate(
-        'GET',
+        `GET`,
         apiURL,
         authParams,
         api.secret
     )
 
+    console.log(signature)
+
     try {
         result = await axios.get(
-            baseUrl + routes.wp + routes.gf + routes.forms + '/' + form.id,
+            baseUrl + routes.wp + routes.gf + routes.forms + `/` + form.id,
             {
-                responseType: 'json',
+                responseType: `json`,
                 params: {
                     ...authParams,
                     oauth_signature: signature,
@@ -81,39 +83,39 @@ async function getFormFields(basicAuth, api, baseUrl, form) {
         return false
     }
 
-    result.data['slug'] = slugify(form.title)
-    result.data['apiURL'] = apiURL
+    result.data[`slug`] = slugify(form.title)
+    result.data[`apiURL`] = apiURL
 
     return result.data
 }
 
 async function getFormsAndFields(basicAuth, api, baseUrl) {
-    let formObj = {}
+    const formObj = {}
 
     // First get forms in list
-    let allForms = await getForms(basicAuth, api, baseUrl)
+    const allForms = await getForms(basicAuth, api, baseUrl)
 
     // If there are forms to move with
     if (allForms) {
         if (!isObjEmpty(allForms)) {
             for (const [key, value] of Object.entries(allForms)) {
                 // Clone form object
-                let currentForm = { ...allForms[key] }
+                const currentForm = { ...allForms[key] }
 
                 // remove unneeded key
                 delete currentForm.entries
 
-                let form = await getFormFields(
+                const form = await getFormFields(
                     basicAuth,
                     api,
                     baseUrl,
                     currentForm
                 )
 
-                formObj['form-' + currentForm.id] = form
+                formObj[`form-` + currentForm.id] = form
             }
         } else {
-            log(chalk.bgRed('We could not find any forms. Have you made any?'))
+            log(chalk.bgRed(`We could not find any forms. Have you made any?`))
         }
 
         return formObj
@@ -125,7 +127,7 @@ function apiErrorHandler(error) {
     if (error.response) {
         // The request was made and the server responded with a status code
         // that falls out of the range of 2xx
-        log(chalk.bgRed('Request was made, but there was an issue'))
+        log(chalk.bgRed(`Request was made, but there was an issue`))
         log(error.response.data)
         log(error.response.status)
         log(error.response.headers)
@@ -133,12 +135,12 @@ function apiErrorHandler(error) {
         // The request was made but no response was received
         // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
         // http.ClientRequest in node.js
-        log(chalk.bgRed('Request was made, but no response'))
+        log(chalk.bgRed(`Request was made, but no response`))
         log(error.request)
     } else {
         // Something happened in setting up the request that triggered an Error
-        log(chalk.bgRed('Something happened setting up the request'))
-        log('Error', error)
+        log(chalk.bgRed(`Something happened setting up the request`))
+        log(`Error`, error)
     }
 }
 
